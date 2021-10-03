e375ec32c362c6d659ed649c87363313919c06d1
Sora Khan <sora.khan@saphi.com.au>
2021-10-03 19:42:45 +1100

    Started playing with GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby-tutorial",
+    title: "Gatsby Site Tutorial",
   },
   plugins: [
     "gatsby-plugin-image",
