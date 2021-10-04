1af3a929b0e4171ba3a56e0f2a24d5217261b127
Sora Khan <sora.khan@saphi.com.au>
2021-10-04 11:33:52 +1100

    Displaying markdown content using transformer plugin gatsby-plugin-mdx

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,27 +1,34 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 function BlogPage({ data }) {
   return (
-    <Layout pageTitle="My Blog">
-      <ul>
-        {data.allFile.nodes.map((node) => {
-          return node.absolutePath.includes(".mdx") ? (
-            <li key={node.name}>{node.name}</li>
-          ) : null;
-        })}
-      </ul>
+    <Layout pageTitle="My Blog Posts">
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>
+            {node.body}
+          </MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 }
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { order: DESC, fields: frontmatter___date }) {
       nodes {
-        name
-        absolutePath
+        body
+        id
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
       }
     }
   }
